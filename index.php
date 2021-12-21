f3bf6acec22d464167dc515e6db0d029d0ea76e4
gbelda <gbelda56@gmail.com>
2021-12-21 16:52:22 +0100

    add addcard function

--- index.php
+++ index.php
@@ -6,7 +6,21 @@ class Product{
     public $isInStock = true;
 }
 
-class User{
+class CreditCard{
+    protected $cardNumber;
+    protected $cardExpDate;
+    protected $cvv;
+
+    function __construct(int $cardNumber, int $cardExpDate, int $cvv)
+    {
+        $this->cardNumber = $cardNumber;
+        $this->cardExpDate = $cardExpDate;
+        $this->cvv = $cvv;
+    }
+
+
+}
+class User extends CreditCard{
     public $name;
     public $lastName;
     public $email;
@@ -17,9 +31,22 @@ class User{
         $this->lastName = $lastName;
         $this->email = $email;
     }
+
+        public function AddCard($card){
+        $this->cardNumber = $card->cardNumber;
+        $this->cardExpDate = $card->cardExpDate;
+        $this->cvv = $card->cvv;
+    }
 }
 
 class PremiumUser extends User{
+
+
     public $discount = 10;
 
 }
+
+$user_1 = new User('giovanni', 'belda', 'gbelda@gmail.com');
+$c = new CreditCard(12345678, 1022 ,233);
+$user_1->AddCard($c);
+var_dump($user_1);
